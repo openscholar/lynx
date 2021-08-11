74d45b47af5204dd696101631a5b568da34dd384
uttkarsh@axelerant.com <uttkarsh@axelerant.com>
2021-08-11 11:02:38 +0530

    Lynx request subscriber.

---
+++ src/EventSubscriber/LynxRequestSubscriber.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Drupal\os\EventSubscriber;
+
+use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Routing\RequestHelper;
+use Drupal\Core\Routing\TrustedRedirectResponse;
+use Drupal\vsite\Plugin\VsiteContextManager;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Symfony\Component\HttpKernel\Event\GetResponseEvent;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+/**
+ * Class OsFrontPageRequestSubscriber.
+ */
+class LynxRequestSubscriber implements EventSubscriberInterface {
+
+  /**
+   * Module specific configuration.
+   *
+   * @var \Drupal\Core\Config\Config
+   */
+  protected $config;
+
+  /**
+   * OsFrontPageRequestSubscriber constructor.
+   *
+   * @param \Drupal\vsite\Plugin\VsiteContextManager $vsiteContextManager
+   *   VsiteContextManager instance.
+   * @param \Drupal\Core\Config\ConfigFactoryInterface $config
+   *   ConfigFactoryInterface instance.
+   */
+  public function __construct(VsiteContextManager $vsiteContextManager, ConfigFactoryInterface $config) {
+    $this->config = $config;
+    $this->lynxPath = $this->config->get('lynx.settings')->get('path');
+    $this->lynxDomain = $this->config->get('lynx.domain.settings')->get('lynx_domain');
+  }
+
+  /**
+   * Performs an internal redirect to lynx search page.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+   *   The Event to process.
+   */
+  public function onKernelRequestLynx(GetResponseEvent $event) {
+    $request = $event->getRequest();
+    $host = $request->getHttpHost();
+    $uri = $request->getPathInfo();
+
+    if ($this->lynxDomain && $host == $this->lynxDomain && strpos($uri, $this->lynxPath) !== 0) {
+      $newrequest = $request->duplicate();
+      $newrequest->server->set('REQUEST_URI', $this->lynxPath);
+      $response = $event->getKernel()->handle($newrequest, HttpKernelInterface::SUB_REQUEST);
+      if ($response) {
+        $event->setResponse($response);
+      }
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSubscribedEvents() {
+    // We need highest priority for this.
+    $events[KernelEvents::REQUEST][] = ['onKernelRequestLynx', 1000];
+    return $events;
+  }
+
+}
